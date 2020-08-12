d89ee8a03f21b06d140605fc12447ef08f6a519b
Gustavo P Borges <gustavo.borges@qualirede.com.br>
2020-08-12 11:13:41 -0300

    chore: add treadment in case envs fail to load

--- cmd/main.go
+++ cmd/main.go
@@ -2,12 +2,17 @@ package main
 
 import (
 	"delivery-much-api/api"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
+		log.Println("Error loading environment variables", err)
 		panic(err)
 	}
 
